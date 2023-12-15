cc228c0424f354eedd9e05a378375513f2e0a89a
NurisSabilaFirdausita <firda9077@gmail.com>
2023-12-15 12:30:37 +0700

    first commit

--- app.py
+++ app.py
@@ -12,31 +12,26 @@ model = load_model('D:\SMT 7\Pembelajaran Mesin\Modul\Modul 6 - AI Web Model Dep
 
 model.make_predict_function()
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def main():
 	return render_template("index.html")
 
-@app.route("/about")
-def about_page():
-	return "Please subscribe  Artificial Intelligence Hub..!!!"
-
-@app.route("/submit", methods = ['GET', 'POST'])
+@app.route("/submit", methods = ['POST'])
 def get_output():
-	if request.method == 'POST':
-		start_time = time.time()
-		img = request.files['my_image']
-		img_path = "static/" + img.filename	
-		img.save(img_path)
-		img_array = image.img_to_array(image.load_img(img_path, target_size=(128, 128))) / 255.0
-		img_array = img_array.reshape(1,128, 128,3)
-		p = model.predict(img_array)
-		predicted_class = np.argmax(p)
-		prediction = dic[predicted_class]
-		confidence = np.round(float(p[0][predicted_class])*100,2)
-		end_time = time.time()
-		runtime = round(end_time - start_time, 4)
-		return render_template("index.html", prediction = prediction,  img_path = img_path, runtime=runtime, confidence=confidence)
-	return render_template("index.html", prediction=None, img_path=None, runtime=None, confidence=None)
+	start_time = time.time()
+	img = request.files['my_image']
+	img_path = "static/" + img.filename	
+	img.save(img_path)
+	img_array = image.img_to_array(image.load_img(img_path, target_size=(128, 128))) / 255.0
+	img_array = img_array.reshape(1,128, 128,3)
+	p = model.predict(img_array)
+	predicted_class = np.argmax(p)
+	prediction = dic[predicted_class]
+	confidence = np.round(float(p[0][predicted_class])*100,2)
+	end_time = time.time()
+	runtime = round(end_time - start_time, 4)
+	return render_template("index.html", prediction = prediction,  img_path = img_path, runtime=runtime, confidence=confidence)
+	
 
 if __name__ =='__main__':
 	app.run(debug = True)
